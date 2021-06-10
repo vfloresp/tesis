adf7a46641769d0b9b459c6a0c8fc88d90f06bb6
Victor Flores <vfloresp@pop-os.localdomain>
2021-06-09 21:38:06 -0500

    primera version del procesamiento de datos

--- src/audio_processing.py
+++ src/audio_processing.py
@@ -1,10 +1,13 @@
 import os
 import pickle
+from tutoriales.preprocess import SPECTROGRAMS_SAVE_DIR
 import librosa
 import numpy as np
 
 
-class LoadFile:
+class Loader:
+    """Load audio files given certain parameters"""
+
     def __init__(self, sample_rate, duration, mono):
         self.sample_rate = sample_rate
         self.duration = duration
@@ -21,6 +24,8 @@ class LoadFile:
 
 
 class Padder:
+    """Adds padding to array"""
+
     def __init__(self, mode="constant"):
         self.mode = mode
 
@@ -34,6 +39,8 @@ class Padder:
 
 
 class LogSpectrogramExtractor:
+    """Obtains the logaritmic spectogram of a signal"""
+
     def __init__(self, frame_size, hop_length):
         self.frame_size = frame_size
         self.hop_length = hop_length
@@ -61,3 +68,105 @@ class MinMaxNormaliser:
         array = (norm_array - self.min) / (self.max - self.min)
         array = array * (original_max - original_min) + original_min
         return array
+
+
+class Saver:
+    def __init__(self, feature_save_dir, min_max_values_save_dir):
+        self.feature_save_dir = feature_save_dir
+        self.min_max_values_save_dir = min_max_values_save_dir
+
+    def save_feature(self, feature, filepath):
+        save_path = self._generate_save_path(filepath)
+        np.save(save_path, feature)
+
+    def save_min_max_values(self, min_max_values):
+        save_path = os.path.join(self.feature_save_dir, "min_max_values.pkl")
+        self._save(min_max_values, save_path)
+
+    @staticmethod
+    def _save(self, data, save_path):
+        with open(save_path, "wb") as f:
+            pickle.dump(data, f)
+
+    def _generate_save_path(self, file_path):
+        file_name = os.path.split(file_path)[1]
+        save_path = os.path.join(self.feature_save_dir, file_name + ".npy")
+        return save_path
+
+
+class PreprocessingAudioPipeline:
+    def __init__(self):
+        self._loader = None
+        self.padder = None
+        self.generator = None
+        self.normaliser = None
+        self.saver = None
+        self.min_max_values = {}
+        self._num_expected_samples = None
+
+    @property
+    def loader(self):
+        return self._loader
+
+    @loader.setter
+    def loader(self, loader):
+        self._loader = loader
+        self._num_expected_samples = int(loader.sample_rate * loader.duration)
+
+    def process(self, audio_files_dir):
+        for master_root, _, directory in os.walk(audio_files_dir):
+            for root, _, files in os.walk(directory):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    self._process_file(file_path)
+                    print(f"Processed file {file_path}")
+        self.saver.save_min_max_values(self.min_max_values)
+
+    def _process_file(self, file_path):
+        signal = self.loader.load(file_path)
+        if self._is_padding_neccesary(signal):
+            signal = self._apply_padding(signal)
+        feature = self.extractor.generate_spectrogram(signal)
+        norm_feature = self.normaliser.normalise(feature)
+        save_path = self.saver.save_feature(norm_feature, file_path)
+        self._store_min_max_value(save_path, feature.min(), feature.max())
+
+    def _is_padding_neccesary(self, signal):
+        if len(signal) < self._num_expected_samples:
+            return True
+        return False
+
+    def _apply_padding(self, signal):
+        num_missing_samples = self._num_expected_samples - len(signal)
+        padded_signal = self.padder.right_pad(signal, num_missing_samples)
+        return padded_signal
+
+    def _store_min_max_value(self, save_path, min_val, max_val):
+        self.min_max_values[save_path] = {"min": min_val, "max": max_val}
+
+
+if __name__ == "__main__":
+    FRAME_SIZE = 512
+    HOP_LENGTH = 256
+    DURATION = 0.74
+    SAMPLE_RATE = 22050
+    MONO = True
+
+    SPECTROGRAMS_SAVE_DIR = "/home/vfloresp/Documents/tesis/tesis/src"
+    MIN_MAX_VALUES_SAVE_DIR = "/home/vfloresp/Documents/tesis/tesis/src"
+    FILES_DIR = "/home/vfloresp/Documents/tesis/tesis/fma/data/fma_medium"
+
+    loader = Loader(SAMPLE_RATE, DURATION, MONO)
+    padder = Padder()
+    log_spectrogram_extractor = LogSpectrogramExtractor(FRAME_SIZE, HOP_LENGTH)
+    min_max_normaliser = MinMaxNormaliser(0, 1)
+    saver = Saver(SPECTROGRAMS_SAVE_DIR, MIN_MAX_VALUES_SAVE_DIR)
+
+    preprocessing_pipeline = PreprocessingAudioPipeline()
+    preprocessing_pipeline.loader = loader
+    preprocessing_pipeline.padder = padder
+    preprocessing_pipeline.extractor = log_spectrogram_extractor
+    preprocessing_pipeline.normaliser = min_max_normaliser
+    preprocessing_pipeline.saver = saver
+
+    preprocessing_pipeline.process(FILES_DIR)
