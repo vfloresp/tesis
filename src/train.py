35da9eda37a9af5326216c1b24ee686f329779af
Victor Flores <vfloresp@pop-os.localdomain>
2021-07-12 22:59:18 -0500

    correcciones train

--- src/train.py
+++ src/train.py
@@ -3,8 +3,8 @@ import numpy as np
 from va_autoencoder import VAE
 
 LEARNING_RATE = 0.0005
-BATCH_SIZE = 32
-EPOCHS = 20
+BATCH_SIZE = 64
+EPOCHS = 150
 
 
 def load_fsdd(spectrogram_path):
@@ -14,27 +14,29 @@ def load_fsdd(spectrogram_path):
             file_path = os.path.join(root, file_name)  # (n_bins, n_frames)
             spectrogram = np.load(file_path)
             x_train.append(spectrogram)
+            print(spectrogram)
     x_train = np.array(x_train)
     x_train = x_train[..., np.newaxis]  # -> (3000,256,64,1)
+    return x_train
 
 
 def train(x_train, learning_rate, batch_size, epochs):
     autoencoder = VAE(
         input_shape=(256, 64, 1),
-        conv_filters=(512, 256, 128, 32),
-        conv_kernels=(3, 3, 3, 3),
-        conv_strides=(1, 2, 2, 1),
-        latent_space_dim=2,
+        conv_filters=(512, 256, 128, 64, 32),
+        conv_kernels=(3, 3, 3, 3, 3),
+        conv_strides=(2, 2, 2, 2, (2, 1)),
+        latent_space_dim=128,
     )
     autoencoder.summary()
     autoencoder.compile(learning_rate)
-    autoencoder.train(x_train[:500], batch_size, epochs)
+    autoencoder.train(x_train, batch_size, epochs)
     return autoencoder
 
 
 if __name__ == "__main__":
-    SPECTROGRAM_PATH = "/home/vfloresp/Documents/tesis/tesis/src/spectrograms"
-    x_train, _, _, _ = load_fsdd(SPECTROGRAM_PATH)
+    SPECTROGRAM_PATH = "/home/vfloresp/Documents/tesis/src/spectrograms"
+    x_train = load_fsdd(SPECTROGRAM_PATH)
     autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
     autoencoder.save("model")
     autoencoder2 = VAE.load("model")
